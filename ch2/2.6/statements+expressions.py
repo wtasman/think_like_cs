7f0eb4802fdc0b650cab616a47bf9da5b23c372a
William Asman <wtasman@Williams-MacBook-Pro.local>
2017-07-16 15:01:07 -0700

    understanding assignments and statements

---
+++ ch2/2.6/statements+expressions.py
@@ -0,0 +1,9 @@
+print(1 + 1)			#1+1 and len("hello") are EXPRESSIONS
+print(len("hello"))		#the value inside the braces needs evaluated
+				#thats what makes them EXPRESSIONS
+y = 3.14			#these are assignment STATEMENTS
+x = len("hello")		#variables can be assigned expressions too
+print(x)
+print(y)
+
+

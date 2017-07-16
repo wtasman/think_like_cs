404c88489f29048232169c36d10bfbc696392b7c
William Asman <wtasman@Williams-MacBook-Pro.local>
2017-07-16 14:11:53 -0700

    practiced converting data_types/classes in python

--- ch2/2.3/convert_type.py
+++ ch2/2.3/convert_type.py
@@ -1,1 +1,15 @@
+print(3.14, int(3.14))
+print(3.9999, int(3.9999))
+print(3.0, int(3.0))
+print(-3.9999, int(-3.9999))
 
+print("2345", int("2345"))
+print(17, int(17))
+print(int("23"))
+
+print(float("123.45"))
+print(type(float("123.45")))
+
+print(str(17))
+print(str(17.42))
+print(type(str(42)))

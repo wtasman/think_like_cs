f26434d57ae455f37a4a51d05f273199875c6941
William Asman <wtasman@Williams-MacBook-Pro.local>
2017-07-16 14:48:28 -0700

    added comments to assignment.py

--- ch2/2.4/assignments.py
+++ ch2/2.4/assignments.py
@@ -1,11 +1,14 @@
-message = "What's up, Doc?"
-n = 17
+message = "What's up, Doc?"	#= means assignment
+n = 17				#here these values are assigned names
 pi = 3.14159
 
-print(message)
-print(n)
+print(message)			#from now on the names and values can
+print(n)			#be used interchangeably
 print(pi)
 
-print(type(message))
+message = "updated message"	#that is until you change it
+print(message)			#then it has a new value
+
+print(type(message))		#variables have types too
 print(type(n))
 print(type(pi))

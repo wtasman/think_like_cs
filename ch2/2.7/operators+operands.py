fc859fbb7793dd7acf3c81611a69d06b49bf26c3
William Asman <wtasman@Williams-MacBook-Pro.local>
2017-07-16 15:22:02 -0700

    did chapter 2.7

---
+++ ch2/2.7/operators+operands.py
@@ -0,0 +1,20 @@
+print(2 + 3)		#+,-,*,**,/,//,% are all operators
+print(2 - 3)		#+ and - work like expected
+print(2 * 3)		#* is for multiplication
+print(2 ** 3)		#** is for exponents
+print(3 ** 2)
+
+minutes = 645
+hours = minutes / 60	#/ is for division that returns a float
+print(hours)
+
+print(7 / 4)
+print(7 // 4)		#// is for addition that truncates to int
+
+minutes = 645
+hours = minutes // 60
+print(hours)
+
+
+remainder = 7 % 3	#% the modulus operator is used to return the remainder
+print(remainder)
